c3ece7876fe6fefe309de4259c26969a2b93fa53
Benjamin Fiorini <benjamin.fiorini@gmail.com>
2018-04-10 13:55:03 +0200

    make datatables responsive

--- src/_assets/js/datatables.js
+++ src/_assets/js/datatables.js
@@ -1,10 +1,13 @@
 import 'datatables.net-zf';
+import 'datatables.net-responsive-zf'
 import 'datatables.net-zf/css/dataTables.foundation.css';
+import 'datatables.net-responsive-zf/css/responsive.foundation.css'
 
 $(document).ready(function () {
   $('#datatables').DataTable({
     "paging": false,
     "info": false,
+    "responsive": true,
     "language": {
       "search": "Recherche :"
     }
