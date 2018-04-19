983083857758541dbf435557471c90d6c330790a
Benjamin Fiorini <benjamin.fiorini@gmail.com>
2018-04-19 09:50:22 +0200

    show fullsize image in lightGallery

--- src/_assets/js/gallery.js
+++ src/_assets/js/gallery.js
@@ -6,11 +6,6 @@ import 'justifiedGallery';
 import '../styles/gallery.scss';
 
 $(document).ready(function () {
-  // Copy data-srcset from image to parent link
-  $('article a[href*="/images/"]').each(function (i) {
-    var srcset = $(this).find('img').attr('data-srcset');
-    $(this).attr('data-srcset', srcset);
-  });
   $("article").lightGallery({
     selector: 'a[href*="/images/"]'
   });
